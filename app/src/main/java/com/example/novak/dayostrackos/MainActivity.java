930e8896840629a2a35012ac86ba357b6fce020d
nov0436 <novakdan5@gmail.com>
2017-11-13 20:57:16 +0100

    First navigation menu changes

--- app/src/main/java/com/example/novak/dayostrackos/MainActivity.java
+++ app/src/main/java/com/example/novak/dayostrackos/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // This may very well be deleted in the future.
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
