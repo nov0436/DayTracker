fcc8332f90d86bb0570ecbb7b067526a53796442
nov0436 <novakdan5@gmail.com>
2017-11-28 21:37:32 +0100

    Date and time setting

--- app/src/main/java/com/example/novak/dayostrackos/WriteNoteActivity.java
+++ app/src/main/java/com/example/novak/dayostrackos/WriteNoteActivity.java
@@ -1,17 +1,33 @@
 package com.example.novak.dayostrackos;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class WriteNoteActivity extends AppCompatActivity implements View.OnClickListener {
+import java.text.DateFormat;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class WriteNoteActivity extends AppCompatActivity implements View.OnClickListener, DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
     static final int GET_DATE_TIME = 1;
 
     TextView dateTimeDisplayTextView;
+    TextView dateTimeHintTextView;
 
+    int year, month, day, hour, minute;
+    int yearFinal, monthFinal, dayFinal, hourFinal, minuteFinal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +37,13 @@ public class WriteNoteActivity extends AppCompatActivity implements View.OnClick
         dateTimeDisplayTextView = (TextView) findViewById(R.id.dateTimeDisplayTextView);
         dateTimeDisplayTextView.setOnClickListener(this);
 
+        dateTimeHintTextView = (TextView) findViewById(R.id.dateTimeTextView);
 
+        String currentDateandTime = new SimpleDateFormat("yyyy-MM-dd   HH:mm").format(new Date());
+        dateTimeDisplayTextView.setText(currentDateandTime);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void onClick(View v) {
         int id = v.getId();
@@ -31,13 +51,43 @@ public class WriteNoteActivity extends AppCompatActivity implements View.OnClick
         switch (id)
         {
             case R.id.dateTimeDisplayTextView:
-                Intent intent = new Intent(this, DateTimeActivity.class);
-                startActivityForResult(intent, GET_DATE_TIME);
+                Calendar c = Calendar.getInstance();
+                year = c.get(Calendar.YEAR);
+                month = c.get(Calendar.MONTH);
+                day  = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(WriteNoteActivity.this, WriteNoteActivity.this, year, month, day);
+                datePickerDialog.show();
                 break;
         }
     }
 
     private void startActivityForResult() {
+
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        yearFinal = year;
+        monthFinal = month + 1;
+        dayFinal = dayOfMonth;
+
+        Calendar c = Calendar.getInstance();
+        hour = c.get(Calendar.HOUR_OF_DAY);
+        minute = c.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(WriteNoteActivity.this, WriteNoteActivity.this,
+                hour, minute, android.text.format.DateFormat.is24HourFormat(this));
+        timePickerDialog.show();
+
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        hourFinal = hourOfDay;
+        minuteFinal = minute;
+
+        dateTimeDisplayTextView.setText(String.format("%d-%d-%d   %d:%d", yearFinal, monthFinal, dayFinal, hourFinal, minuteFinal));
 
     }
 }
